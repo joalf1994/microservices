d47605c53902846e7e7cc159c86d001e4425e86a
Jbobadilla <joalf1994@gmail.com>
2026-03-06 16:41:21 -0500

    test: implementación de test uniotarios en la capa controller
    - Agregación del método getMockInventoryResponse q mapea los datos de
      respuesta para pruebas
    - Implementación de 3 test unitarios para cada método.

--- inventory-service/src/test/java/com/jbobadilla/inventory_service/util/InventoryDataFactory.java
+++ inventory-service/src/test/java/com/jbobadilla/inventory_service/util/InventoryDataFactory.java
@@ -1,5 +1,6 @@
 package com.jbobadilla.inventory_service.util;
 
+import com.jbobadilla.inventory_service.model.dto.InventoryResponse;
 import com.jbobadilla.inventory_service.model.dto.OrderItemRequest;
 import com.jbobadilla.inventory_service.model.entity.Inventory;
 
@@ -14,6 +15,13 @@ public class InventoryDataFactory {
     public static OrderItemRequest.OrderItemRequestBuilder getMockOrderItemRequest() {
         return OrderItemRequest.builder()
                 .sku("000001")
+                .quantity(10L);
+    }
+
+    public static InventoryResponse.InventoryResponseBuilder getMockInventoryResponse() {
+        return InventoryResponse.builder()
+                .sku("000001")
+                .isInStock(true)
                 .quantity(10L);
     }
 
